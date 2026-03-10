6d61fc9718aa5e1bfed47c45c8404ec6cbf7705b
vinzenzm <vinzenz@malke.info>
2026-03-10 13:24:00 +0100

    Remove quadcopter_model from main repo to add as submodule

--- crazyflie_sitl/quadcopter_model/src/objects/object_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "quadcopter_model/objects/object_base.hpp"
-
-namespace quadcopter {
-
-ObjectBase::ObjectBase() {}
-
-ObjectBase::~ObjectBase() {}
-
-}  // namespace quadcopter
--- crazyflie_sitl/quadcopter_model/src/sensors/sensor_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "quadcopter_model/sensors/sensor_base.hpp"
-
-namespace quadcopter {
-
-SensorBase::SensorBase() {}
-
-SensorBase::~SensorBase() {}
-
-}  // namespace quadcopter
